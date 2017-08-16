4935e95606cfa8c5431b92ff3eb73b23499a0364
similun1986 <31058151+similun1986@users.noreply.github.com>
2017-08-16 16:52:03 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,9 @@
 <?php
 $access_token = 'sbQaa0iwGt6ilgF8l/KCeC9+ccsq8v/voUh53Sc2+Kv7Nbl0YjjHJ6jpxdbP4fWP7LAlu1luYN/1tmEGmEu6dE+OsMrBOFAcLqqfKJ//A5yoUTnEIOjn9T8EJpo1pvk2ztcx5i9680b2k0ahRgrppgdB04t89/1O/w1cDnyilFU=';
 
+$proxy = 'http://fixie:0jMECBPPUU6WDu9@velodrome.usefixie.com:80';
+$proxyauth = 'http://fixie:0jMECBPPUU6WDu9@velodrome.usefixie.com:80';
+
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
@@ -9,6 +12,10 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
 $result = curl_exec($ch);
 curl_close($ch);
 
