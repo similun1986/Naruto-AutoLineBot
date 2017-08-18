1af66ccab47f92b79c11ea2050eaded9f8b75e81
similun1986 <31058151+similun1986@users.noreply.github.com>
2017-08-18 13:57:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['text'] "ส่งมาจาก :"['userid'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
@@ -25,14 +25,14 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' => $text
-			];
+			           ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
-			];
+			        ];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
