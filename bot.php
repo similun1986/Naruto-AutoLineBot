8fc9c230aae28c88c90ddc4fff03e7627f9e9166
similun1986 <31058151+similun1986@users.noreply.github.com>
2017-08-18 17:29:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages]						
+				'messages' => [$messages] " ID คือ" ['source']['userId']					
 			        ];
 			
 			
