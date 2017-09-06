cd6a48790a473338c4d94c134271d66f0426b157
similun1986 <31058151+similun1986@users.noreply.github.com>
2017-09-06 16:34:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			$userid = $event['source']['userid'];
+			$userid = $event['message']['userid'];
 			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [					
 					'type' => 'text',
-					'text' => $text												
+					'text' => $text	": Hellow" $userid											
 			            ];
 			
 			// Make a POST Request to Messaging API to reply to sender
