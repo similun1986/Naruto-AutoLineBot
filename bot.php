640005625ebb313a6b2c91336947e56b8c9949cf
similun1986 <31058151+similun1986@users.noreply.github.com>
2017-09-06 16:49:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,8 +25,10 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [					
+				{
 					'type' => 'text',
-					'text' => $userid
+					'text' => $text 
+				 }
 			            ];
 			
 			// Make a POST Request to Messaging API to reply to sender
