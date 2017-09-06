ab77174f57b9f4e1e405ff499067b0f6cb1816de
similun1986 <31058151+similun1986@users.noreply.github.com>
2017-09-06 16:53:20 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,17 +25,24 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [					
-				{
+				
 					'type' => 'text',
 					'text' => $text 
-				 }
+				
+			            ];
+			
+			$messages2 = [					
+				
+					'type' => 'text',
+					'text' => "Hello Tonmint"
+				
 			            ];
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages] 					
+				'messages' => [$messages] ,[$messages2]					
 			        ];
 			
 			
